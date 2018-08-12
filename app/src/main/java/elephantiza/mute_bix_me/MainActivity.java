01689093d3ffd947a44b354c877ab0f90fb26ecb
iunodora <tiz20@cam.ac.uk>
2018-08-12 12:08:17 +0100

    Initial commit.

---
+++ app/src/main/java/elephantiza/mute_bix_me/MainActivity.java
@@ -0,0 +1,13 @@
+package elephantiza.mute_bix_me;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
